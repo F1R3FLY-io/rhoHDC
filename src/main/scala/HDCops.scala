f3265ef60bd70e2be16c76cb5545aac513db7896
Lucius Meredith <lgreg.meredith@gmail.com>
2023-08-28 17:55:39 -0700

    added term target for compilation

--- src/main/scala/HDCops.scala
+++ src/main/scala/HDCops.scala
@@ -4,7 +4,15 @@ import breeze.linalg.{DenseVector, SparseVector}
 import breeze.stats.distributions._
 import breeze.stats.distributions.Rand.FixedSeed.randBasis
 
-object HVAlgebra {
+trait HVT[V[_],Q] {
+  def zero() : V[Q]
+  def rand(): V[Q]
+  def xOr(v1: V[Q], v2: V[Q]): V[Q]
+  def perm(v1: V[Q], v2: V[Q]): V[Q]
+  def maj(summands: List[V[Q]]): V[Q]
+}
+
+object HVAlgebra extends HVT[SparseVector, Boolean] {
   val defaultDimension : Int = 4072
   val Bernie = Bernoulli.distribution(0.5)
   val Uni = Uniform(0,defaultDimension-1)
@@ -18,4 +26,31 @@ object HVAlgebra {
   def perm(v1: SparseVector[Boolean], v2: SparseVector[Boolean]): SparseVector[Boolean] = ??? //v2.permute(v1.toDenseVector)
   def maj(summands: List[SparseVector[Boolean]]): SparseVector[Boolean] =
     ??? //summands.reduce((a, b) => a + b).mapValues(_ >= (vectors.size / 2))
+}
+
+trait HVExpr[V]
+case class HVXor[V](l: HVExpr[V], r: HVExpr[V])
+    extends HVExpr[V]
+case class HVPerm[V](perm: HVExpr[V], hvec: HVExpr[V])
+    extends HVExpr[V]
+case class HVMaj[V](summands: List[HVExpr[V]])
+    extends HVExpr[V]
+
+object HVTermAlgebra extends HVT[HVExpr,Boolean] {
+  case object HV0 extends HVExpr[Boolean]
+  case object HV1 extends HVExpr[Boolean]
+  case object HVRand extends HVExpr[Boolean]
+  def zero() : HVExpr[Boolean] = HV0
+  def rand(): HVExpr[Boolean] = HV1
+  def xOr(
+    v1: HVExpr[Boolean],
+    v2: HVExpr[Boolean]
+  ): HVExpr[Boolean] = HVXor[Boolean]( v1, v2 )
+  def perm(
+    v1: HVExpr[Boolean],
+    v2: HVExpr[Boolean]
+  ): HVExpr[Boolean] = HVPerm[Boolean]( v1, v2 )
+  def maj(summands: List[HVExpr[Boolean]]): HVExpr[Boolean] = {
+    HVMaj[Boolean]( summands )
+  }
 }
