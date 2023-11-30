a7f80b0995be7ff8f5534c2c8735de6758561bbd
Lucius Meredith <lgreg.meredith@gmail.com>
2023-11-29 19:27:14 -0800

    added HDCExpr salt

--- hdc/src/main/scala/examples.scala
+++ hdc/src/main/scala/examples.scala
@@ -3,4 +3,6 @@ package io.f1r3fly.rhohdc.tinyrho
 object HDCopsExamples {
   val salt1 = HVAlgebra.rand()
   val salt2 = HVAlgebra.rand()
+  val salt3 = HVTermAlgebra.rand()
+  val salt4 = HVTermAlgebra.rand()
 }
